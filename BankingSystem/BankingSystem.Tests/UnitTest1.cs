fa84168fa4311d951aa177eaff42d1e8e4de69be
Themselfagain <sheikysmar7y@gmail.com>
2023-02-16 10:21:14 +0200

    Unit testing done

--- BankingSystem/BankingSystem.Tests/UnitTest1.cs
+++ BankingSystem/BankingSystem.Tests/UnitTest1.cs
@@ -1,9 +1,46 @@
 using NUnit.Framework;
 using BankingSystem;
+using System;
+
 namespace BankingSystem.Tests
 {
     public class Tests
     {
+        [Test]
+        public void ConstructorShouldWork()
+        {
+            int id = 123;
+            BankAccount bankAccount = new BankAccount(id);
+            Assert.AreEqual(id, bankAccount.Id);
+        
+        }
+        [Test]
+        [TestCase (-1,512)]
+        [TestCase (-2,123)]
+        public void DepositShouldThrow(decimal deposit, int id)
+        {
+            
+            BankAccount bankAccount = new BankAccount(id);
+            Assert.Throws<InvalidOperationException>(
+                ()=>bankAccount.Deposit(deposit)) ;
+        }
+        [Test]
+        public void BalanceShouldBeZero()
+        {
+            BankAccount bankAccount = new BankAccount(123);
+            double balance = 0;
+            Assert.AreEqual(balance, bankAccount.Balance);
+        }
+        [Test]
+        public void DepositShouldThrowMessage()
+        {
+            decimal deposit = -1;
+            BankAccount bankAccount = new BankAccount(123);
+            string exp = "Negative Amount";
+            var act = Assert.Throws<InvalidOperationException>(
+                () => bankAccount.Deposit(deposit));
+            Assert.AreEqual(exp, act.Message);
+        }
         [Test]
         public void DepositShouldIncreaseBalance()
         {
@@ -12,18 +49,15 @@ namespace BankingSystem.Tests
             bankAccount.Deposit(depositAmount);
             Assert.AreEqual(depositAmount, bankAccount.Balance);
         }
+        [Test]
         public void AccountInicializeWithPositiveValues()
         {
             BankAccount bankAccount = new BankAccount(123, 2000m);
             Assert.AreEqual(2000m, bankAccount.Balance);
-            
-            
-        }
-        public void Test1()
-        {
-            Assert.Pass();
-           
+
 
         }
+
+
     }
 }
